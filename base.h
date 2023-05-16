cd0f9c4a3baf5481ac082cda692a95889cba59af
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-05-16 09:51:39 +0700

    Added limit constants, backward compatible

--- base.h
+++ base.h
@@ -2,7 +2,7 @@
 #ifndef BASE_H
 #define BASE_H
  
-/////////////////////////
+//////////////////////////////////
 // NOTE: Context Cracking
 
 #if defined(__clang__)
@@ -115,14 +115,33 @@
 # define ENABLE_ASSERT 0
 #endif
 
-/////////////////////////
+//////////////////////////////////
+// NOTE: Basic Types
+
+#include <stdint.h>
+typedef int8_t   S8;
+typedef int16_t  S16;
+typedef int32_t  S32;
+typedef int64_t  S64;
+typedef uint8_t  U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint64_t U64;
+typedef S8       B8;
+typedef S16      B16;
+typedef S32      B32;
+typedef S64      B64;
+typedef float    F32;
+typedef double   F64;
+
+//////////////////////////////////
 // NOTE: Helper Macros
 
 #define Stmnt(s) do { s } while(0)
 
 // NOTE: Not great can be overidden later
 #if !defined(AssertBreak)
-# define AssertBreak() *(static_cast<int*>(nullptr)) = 0;
+# define AssertBreak() *((int*)0) = 0;
 #endif
 
 #if ENABLE_ASSERT
@@ -130,5 +149,29 @@
 #else 
 # define Assert(c)
 #endif
+
+#define global static
+
+//////////////////////////////////
+// NOTE: Limit Constants
+const global S8  min_S8  = (S8)  0x80;
+const global S16 min_S16 = (S16) 0x8000;
+const global S32 min_S32 = (S32) 0x80000000;
+const global S64 min_S64 = (S64) 0x8000000000000000llu;
+
+const global S8  max_S8  = (S8)  0x7f;
+const global S16 max_S16 = (S16) 0x7fff;
+const global S32 max_S32 = (S32) 0x7fffffff;
+const global S64 max_S64 = (S64) 0x7fffffffffffffffllu;
+
+const global U8  max_U8  = 0xff;
+const global U16 max_U16 = 0xffff;
+const global U32 max_U32 = 0xffffffff;
+const global U64 max_U64 = 0xffffffffffffffffllu;
+
+global uint8_t  min_U8;
+global uint16_t min_U16;
+global uint32_t min_U32;
+global uint64_t min_U64;
 
 #endif // BASE_H
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h> // Will remove this later.
+#include "base.h"
+
+#define EvalPrint(x) printf("%s = %d\n", #x, (S32)(x))
+#define EvalPrintLL(x) printf("%s = %ld\n", #x, (S64)(x))
+#define EvalPrintU(x) printf("%s = %u\n", #x, (U32)(x))
+#define EvalPrintULL(x) printf("%s = %lu\n", #x, (U64)(x))
+
+int main()
+{
+  printf("[Context]\n");
+  printf("COMPILER_CL    %d\n", COMPILER_CL   );
+  printf("COMPILER_CLANG %d\n", COMPILER_CLANG);
+  printf("COMPILER_GCC   %d\n", COMPILER_GCC  );
+  printf("OS_WINDOWS     %d\n", OS_WINDOWS    );
+  printf("OS_LINUX       %d\n", OS_LINUX      );
+  printf("OS_MAC         %d\n", OS_MAC        );
+  printf("ARCH_X64       %d\n", ARCH_X64      );
+  printf("ARCH_X86       %d\n", ARCH_X86      );
+  printf("ARCH_ARM       %d\n", ARCH_ARM      );
+  printf("ARCH_ARM64     %d\n", ARCH_ARM64    );
+  Assert(1 > 100);
+
+
+  printf("\n[Limit Constants]\n");
+  // Minimum Limits (Signed)
+  EvalPrint(min_S8);
+  EvalPrint(min_S16);
+  EvalPrint(min_S32);
+  EvalPrintLL(min_S64);
+
+  // Maximum Limits (Signed)
+  EvalPrint(max_S8);
+  EvalPrint(max_S16);
+  EvalPrint(max_S32);
+  EvalPrintLL(max_S64);
+
+  // Maxmimum Limits (Unsigned)
+  EvalPrintU(max_U8);
+  EvalPrintU(max_U16);
+  EvalPrintU(max_U32);
+  EvalPrintULL(max_U64);
+  return 0; 
+}
